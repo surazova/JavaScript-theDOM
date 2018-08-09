6fc562d613c6af41858bdf2ea9a58454cc44e21c
Sitora Urazova <sitoraurazova@gmail.com>
2018-08-09 22:47:11 +0000

    Lessons 7-9 JS and the DOM

--- app.js
+++ app.js
@@ -1,14 +1,62 @@
-const titles = document.getElementsByClassName('title');
+//Lessons 7 JS and the DOM
 
-// console.log(Array.isArray(titles));
-// console.log(Array.isArray(Array.from(titles))); //Creates an array from the titles HTML collection 
+// const bookList = document.querySelector('#book-list');
 
-// Array.from(titles).forEach(function(title){
-//   console.log(title);
-// });
+// console.log('the parent node is:', bookList.parentNode);
+// console.log('the parent element is:', bookList.parentElement.parentElement);
+
+// console.log(bookList.children);
 
 
-Array.from(titles).forEach(function(item){
-    console.log(item);
-})
-//The above doesn't work because you're using a funciton on html, which doesn't work. 
+
+//Lesson 8 
+
+// const bookList = document.querySelector('#book-list');
+
+// console.log('book-list next sibling is:', bookList.nextSibling);
+// console.log('book-list next element sibling is:', bookList.nextElementSibling);
+
+// console.log('book-list previous sibling is:', bookList.previousSibling);
+// console.log('book-list previous element sibling is:', bookList.previousElementSibling);
+
+// bookList.previousElementSibling.querySelector('p').innerHTML += '<br/>Too cool for everyone else!';
+// //(We grabbed the previous Sibling, then used the querySelector to change the HTML)
+
+//Lesson 9 
+//Creating more event listeners 
+//First, query the DOM to grab what you need an event listener for 
+
+//Changing the function of the delete button 
+var btns = document.querySelectorAll('#book-list.delete');
+
+Array.from(btns).forEach(function(btn){
+   btn.addEventListener('click', function(e){
+       
+       
+       const li = e.target.parentElement;
+       li.parentNode.removeChild(li)
+   }); 
+});
+
+
+const listItems = document.querySelectorAll('#book-list ul li');
+
+Array.from(listItems).forEach(function(item){
+  item.addEventListener('click', (e) => {
+
+    const li = e.target.parentElement;
+    console.log('child element to remove:', li);
+    console.log('parent element to remove child from:', li.parentElement);
+    li.parentNode.removeChild(li);
+
+  });
+});
+
+// prevent default behaviour
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', function(e){
+  e.preventDefault();
+  console.log('Navigation to', e.target.textContent, 'was prevented');
+});
