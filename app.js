0953f62684bc0fddfcbbc636e3d23c89db0a3a85
Sitora Urazova <sitoraurazova@gmail.com>
2018-08-14 22:25:45 +0000

    Lesson 17 JS and the DOM

--- app.js
+++ app.js
@@ -85,3 +85,21 @@ searchBar.addEventListener('keyup', function(e){
         }
     })
 })
+
+
+//Lesson 17
+//Creating tabbed content 
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', function(e){
+    if(e.target.tagName == "LI"){
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        panels.forEach(function(panel){
+            if (panel == targetPanel){
+                panel.classList.add('active');
+            } else {
+                panel.classList.remove('active');
+            }
+        })
+    }
+})
